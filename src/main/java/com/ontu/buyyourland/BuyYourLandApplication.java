bea6d7c8ef9a26fc52a0e89c6eed125f3edd525c
PS75215 <p.saha@zensar.com>
2024-11-02 21:02:50 +0530

    intial setup

---
+++ src/main/java/com/ontu/buyyourland/BuyYourLandApplication.java
@@ -0,0 +1,13 @@
+package com.ontu.buyyourland;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BuyYourLandApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BuyYourLandApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ontu/buyyourland/BuyYourLandApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ontu.buyyourland;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BuyYourLandApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
